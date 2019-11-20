a5f0239d5f01efebd71cac6c87f001ee6102030d
Colin Turing <colin.koga@gmail.com>
2019-11-20 07:27:32 -0700

    Add functionality to switch between news categories

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,21 +1,44 @@
 import React, { Component } from 'react';
 import local from '../../data/local';
+import entertainment from '../../data/entertainment';
+import health from '../../data/health';
+import science from '../../data/science';
+import technology from '../../data/technology';
 import './App.css';
 import NewsContainer from '../NewsContainer/NewsContainer';
+import Menu from '../Menu/Menu';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      local
+      local,
+      entertainment,
+      technology,
+      science,
+      health,
+      displayedNews: local,
+      categoryName: local
     }
+  }
+
+  pickCategory = (category) => {
+    this.setState({displayedNews: this.state[category]})
+
+    this.setState({categoryName: this.state[category]})
   }
 
   render () {
     return (
-      <div className="app">
-        <NewsContainer articles={this.state.local}/>
-      </div>
+      <main className="app">
+        <header className="header">
+          <h1>What's New?</h1>
+        </header>
+        <aside>
+          <Menu pickCategory={this.pickCategory}/>
+        </aside>
+        <NewsContainer articles={this.state.displayedNews}/>
+      </main>
     );
   }
 }
