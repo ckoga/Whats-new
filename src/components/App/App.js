cea2c75ac8c8dbec6f6dcd926a36757063959374
Colin Turing <colin.koga@gmail.com>
2019-11-21 07:41:15 -0700

    Add functionality and css install jest and enzyme

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,43 +1,72 @@
 import React, { Component } from 'react';
 import local from '../../data/local';
-import entertainment from '../../data/entertainment';
-import health from '../../data/health';
-import science from '../../data/science';
-import technology from '../../data/technology';
+// import entertainment from '../../data/entertainment';
+// import health from '../../data/health';
+// import science from '../../data/science';
+// import technology from '../../data/technology';
 import './App.css';
 import NewsContainer from '../NewsContainer/NewsContainer';
 import Menu from '../Menu/Menu';
+import SearchForm from '../SearchForm/SearchForm';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      local,
-      entertainment,
-      technology,
-      science,
-      health,
+      news: null,
       displayedNews: local,
       categoryName: local
     }
+    this.fetchData();
+  }
+
+  fetchData = () => {
+    fetch('https://whats-new-api.herokuapp.com/api/v1/news')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ news: data });
+        this.pickCategory('local');
+      })
+      .catch(error => console.log(error))
   }
 
   pickCategory = (category) => {
-    this.setState({displayedNews: this.state[category]})
+    this.setState({displayedNews: this.state.news[category]})
 
-    this.setState({categoryName: this.state[category]})
+    this.setState({categoryName: this.state.news[category]})
+  }
+
+  searchNews = (event) => {
+    event.preventDefault();
+    let inputValue = event.target.closest('form').children[0].value
+    const filteredNews = this.state.displayedNews.filter(news => {
+      let headline = news.headline.toLowerCase();
+      let description = news.description.toLowerCase();
+      
+      if (headline.includes(inputValue) || description.includes(inputValue)) {
+        return true
+      } else {
+        return false
+      }
+    })
+    this.setState({ displayedNews: filteredNews })
   }
 
   render () {
     return (
       <main className="app">
         <header className="header">
-          <h1>What's New?</h1>
+          <h1>What's <span className="new">New?</span></h1>
+          <SearchForm searchNews={this.searchNews}/>
         </header>
-        <aside>
-          <Menu pickCategory={this.pickCategory}/>
-        </aside>
-        <NewsContainer articles={this.state.displayedNews}/>
+        <section>
+          <aside>
+            <Menu pickCategory={this.pickCategory}/>
+          </aside>
+          <div className="article-container">
+            <NewsContainer articles={this.state.displayedNews}/>
+          </div>
+        </section>
       </main>
     );
   }
